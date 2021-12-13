ca39fd18be275fb93d2873dcc343ba3d3b757c14
Your Name <you@example.com>
2021-12-13 20:08:28 -0300

    first commit

---
+++ lógica.js
@@ -0,0 +1,19 @@
+function insert(num) { 
+   let num1=document.getElementById('resultado').innerHTML
+   document.getElementById('resultado').innerHTML=num1+num
+}
+function clean() {
+    document.getElementById('resultado').innerHTML='';
+}
+function back() {
+    let back=document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = back.substring(0, back.length -1);
+}
+function calcular() {
+    let resultado=document.getElementById('resultado').innerHTML
+    if (resultado) {
+        document.getElementById('resultado').innerHTML=eval(resultado);
+    }else{
+        document.getElementById('resultado').innerHTML='Nada para calcular'
+    }
+}
