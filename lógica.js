ddc10ef45ccf839515592abd0906fbd3a94f3e79
Leonardo Santos <93662977+LeonardoSantosBR@users.noreply.github.com>
2021-12-13 22:48:11 -0300

    Update lógica.js

--- lógica.js
+++ lógica.js
@@ -13,7 +13,5 @@ function calcular() {
     let resultado=document.getElementById('resultado').innerHTML
     if (resultado) {
         document.getElementById('resultado').innerHTML=eval(resultado);
-    }else{
-        document.getElementById('resultado').innerHTML='Nada para calcular'
     }
 }
